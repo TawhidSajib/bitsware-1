c36e29f604fd0c6e0f91313fd5b4149056426885
imation82 <18244941+imation82@users.noreply.github.com>
2022-12-24 19:39:10 +0600

    update 1.5.5

---
+++ dist/_nuxt/dadb604.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{283:function(n,e,o){n.exports=o.p+"img/header-ornament-1.e5d3a4a.svg"},284:function(n,e,o){n.exports=o.p+"img/header-ornament-2.755680a.svg"}}]);
