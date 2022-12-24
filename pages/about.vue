8c9de6e70f888bc1eb6ecac73f063b3d37062bdb
imation82 <18244941+imation82@users.noreply.github.com>
2022-12-24 18:47:37 +0600

    About page

--- pages/about.vue
+++ pages/about.vue
@@ -6,7 +6,27 @@
             headlineStyle="About Us!"
             :ornaments="headlineFontStyle"
             subheadline="A brief introduction about Bitsware"
-        ></header-inner>
+        >
+        </header-inner>
+        <div class="about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="about__block position-relative">
+                            <img src="~/assets/images/about-banner.jpg" class="d-block mx-auto img-fluid" alt="About">
+                            <div class="about__block--content mx-auto position-relative">
+                                <subheadline
+                                    title="Who "
+                                    titleStyle="We Are"
+                                    :ornaments="subheaderFontStyle"
+                                ></subheadline>      
+                                <p>Bitsware is a software development team builder based in New York. We are a group of dedicated, experienced designers and developers who can assist you in finding the ideal offshore personnel for the success of your firm. Our network of experienced, knowledgeable, innovative, global personnel is unrivaled in the industry. </p>                       
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <WhyBitsware/>
         <Testimonial/>
         <BorderBottom/>
@@ -24,6 +44,7 @@ import BorderBottom from '~/components/Common/BorderBottom';
 import Gallery from '~/components/Common/Gallery';
 import Footer from '~/components/Footer';
 import headlineFontStyle from '~/assets/svg/about-ornament.svg';
+import subheaderFontStyle from '~/assets/svg/subheader-font-style.svg';
 
 export default {
     name: 'About',
@@ -34,11 +55,13 @@ export default {
         BorderBottom,
         Gallery,
         Footer,
-        "header-inner": () => import("@/components/Common/HeaderInner")
+        "header-inner": () => import("@/components/Common/HeaderInner"),
+        "subheadline": () => import("@/components/Common/Subheadline")
     },    
     data() {
         return {
-            headlineFontStyle
+            headlineFontStyle,
+            subheaderFontStyle
         }
     }
 }
