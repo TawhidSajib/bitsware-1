0abe2fe84fda43106795dd62870b94ee0f5fe1f7
imation82 <18244941+imation82@users.noreply.github.com>
2022-12-24 14:03:56 +0600

    About page

--- pages/about.vue
+++ pages/about.vue
@@ -7,18 +7,27 @@
             :ornaments="headlineFontStyle"
             subheadline="A brief introduction about Bitsware"
         ></header-inner>
+        <WhyBitsware/>
+        <Testimonial/>
+        <Footer/>
     </section>
 </template>
 
 <script>
 
 import Nav from '~/components/Nav';
+import WhyBitsware from '~/components/Common/WhyBitsware';
+import Testimonial from '~/components/Common/Testimonial';
+import Footer from '~/components/Footer';
 import headlineFontStyle from '~/assets/svg/about-ornament.svg';
 
 export default {
     name: 'About',
     components: {
         Nav,
+        WhyBitsware,
+        Testimonial,
+        Footer,
         "header-inner": () => import("@/components/Common/HeaderInner")
     },    
     data() {
