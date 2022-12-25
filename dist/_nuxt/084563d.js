4d799bddc9fe2d6637beafe8bd97fc0ca12f6c8e
imation82 <18244941+imation82@users.noreply.github.com>
2022-12-26 00:51:22 +0600

    Project Single

--- dist/_nuxt/084563d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{282:function(n,e,o){n.exports=o.p+"img/header-ornament-1.e5d3a4a.svg"},283:function(n,e,o){n.exports=o.p+"img/header-ornament-2.755680a.svg"}}]);
