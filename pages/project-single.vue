bae1737947b61ac8332b3c621b208a14008a2315
Ashfaqur Rahman <odesk.rahman@gmail.com>
2022-12-26 10:25:01 +0600

    Project inner page

--- pages/project-single.vue
+++ pages/project-single.vue
@@ -33,8 +33,30 @@
                                 <div class="col-lg-9">
                                     <div class="project__inner--details">
                                         <h2>Smartwatch Design Ideas</h2>
-                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search</p>                                        
-                                    </div>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search</p>
+                                        <div class="project__inner--block">
+                                            <div class="row">
+                                                <div class="col-lg-4">
+                                                    <div class="project__inner--steps">
+                                                        <h4>01. Problem</h4>
+                                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <div class="project__inner--steps">
+                                                        <h4>02. Approach</h4>
+                                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <div class="project__inner--steps">
+                                                        <h4>03. Solution</h4>
+                                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                                                    </div>
+                                                </div>
+                                            </div>                                                
+                                        </div>
+                                    </div>                                    
                                 </div>
                             </div>
                         </div>
